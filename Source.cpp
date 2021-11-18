8d5baa25c64bc0057a2a6ce33d396802b93358d5
ambivalent66k <90841732+ambivalent66k@users.noreply.github.com>
2021-11-18 18:08:27 +0500

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -2,16 +2,17 @@
 using namespace std;
 int main()
 {
-	cout << "Please enter two numbers to find the difference, composition and quotient of their modules" << endl;
+	cout << "Please enter two numbers to find the difference, composition and quotient of their squares." << endl;
 	double a, b, S, P, R, Y, T, E;
 	cin >> a;
 	cin >> b;
-	S = abs(a);
-	P = abs(b);
+	S = a * a;
+	P = b * b;
 	R = S + P;
 	Y = S - P;
 	T = S * P;
 	E = S / P;
 
 	cout << "sum=" << R << endl << "difference=" << Y << endl << "composition=" << T << endl << "quotient=" << E << endl;
+
 }
