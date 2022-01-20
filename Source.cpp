32ea79aca9fce13df5d01f3fbfc41c38f44e47bd
ambivalent66k <90841732+ambivalent66k@users.noreply.github.com>
2022-01-20 18:11:04 +0500

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,17 +1,62 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-	cout << "Please enter two numbers to find the difference, composition and quotient of their modules" << endl;
-	double a, b, S, P, R, Y, T, E;
-	cin >> a;
-	cin >> b;
-	S = abs(a);
-	P = abs(b);
-	R = S + P;
-	Y = S - P;
-	T = S * P;
-	E = S / P;
 
-	cout << "sum=" << R << endl << "difference=" << Y << endl << "composition=" << T << endl << "quotient=" << E << endl;
+#include <iostream>
+
+#include <chrono>
+
+#include <iostream>
+
+#include <ctime>
+
+using namespace std;
+
+void Sort(int* x, int size) { // собсна расчёска
+
+	auto begin = std::chrono::steady_clock::now();
+
+	constexpr double factor = 1.2473309;
+
+	int step = size - 1;
+
+	while (step >= 1) {
+
+		for (int i = 0; i + step < size; i++) {
+
+			if (x[i] > x[i + step]) {
+
+				std::swap(x[i], x[i + step]);
+
+			}
+
+		}
+
+		step /= factor;
+
+	}
+
+	auto end = std::chrono::steady_clock::now();
+
+	auto elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+
+	std::cout << elapsed_ms.count() << " ";
+
+}
+
+int main() {
+
+	for (int SIZE = 1000; SIZE <= 100000; SIZE += 100) {
+
+		int* x = new int[SIZE];
+
+		for (int i = 0; i < SIZE; i++) { //заполнение массива
+
+			x[i] = rand() % 100;
+
+			//cout << x[i];
+
+		}
+
+		Sort(x, SIZE);
+
+	}
+
 }
